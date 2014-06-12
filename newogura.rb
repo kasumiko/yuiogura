ea7da4e9a92159dd7c279a96ad4892fda1db3bb9
kasumiko <ponywoodcommon@gmail.com>
2014-06-13 01:51:04 +0900

    a

--- newogura.rb
+++ newogura.rb
@@ -5,11 +5,14 @@ Plugin.create :newogura do
 if Time.now - m.message[:created] < 5 then
 if m.message.to_s =~ /test|推しプレ/
 num = rand(0..290)
-sol = ""
+sol = []
 File::open("list.txt"){|content|
-sol = content.readlines[num]
+content.each_line{[inline]
+sol << inline
 }
-Service.primary.post(:message =>"#{"@" + m.user.idname} " + sol.to_s, :replyto => m)
+}
+putphoto = sol[num]
+Service.primary.post(:message =>"#{"@" + m.user.idname} " + putphoto.to_s, :replyto => m)
         end
     end
   end
