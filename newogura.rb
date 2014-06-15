5c47394a134f79603e1bda8285ba0794ed8af470
kasumiko <ponywoodcommon@gmail.com>
2014-06-16 03:43:23 +0900

    ゆいおぐらガチャ
    とりあえずこれで完成

--- newogura.rb
+++ newogura.rb
@@ -1,20 +1,32 @@
 # -*- coding: utf-8 -*-
-Plugin.create :newogura do
+Plugin.create(:yuiogura) do
   on_appear do |ms|
     ms.each do |m|
 if Time.now - m.message[:created] < 5 then
-if m.message.to_s =~ /test|推しプレ/
-num = rand(0..290)
-sol = []
-File::open("list.txt"){|content|
-content.each_line{[inline]
-sol << inline
+#ゆいおぐらガチャ(通常)
+if m.message.to_s =~ /((ゆい|唯|ユイ|yui|YUI|ﾕｲ)(ｵｸﾞﾗ|オグラ|ogura|おぐら|OGURA|おぐらちゃん|小倉ちゃん|小倉|ゆい|yui|ﾕｲ)|(ｵｸﾞﾗ|ogura|OGURA|おぐら|小倉|オグラ)(ﾕｲ|ゆい|唯|ユイ|YUI|yui|ゆいちゃん|唯ちゃん|ﾕｲちゃん|ﾕｲﾁｬﾝ)|台乙|台乙先生|ゆい\(\*-v・\)ゆい|唯ちゃん|おぐゆい|だいおつ|だい\(\*-v・\)おつ)(ガチャ|がちゃ|ｶﾞﾁｬ)|がちゃおぐら|EXcmd/
+ open(File.expand_path('../list.txt',__FILE__)) do |f|
+Service.primary.post(:message =>"#{"@" + m.user.idname} " + f.readlines[rand(290)], :replyto => m)
+end 
+end
+#ゆいおぐらガチャ(応援)
+if m.message.to_s =~ /もう(ダメ|だめ)だ/
+open(File.expand_path('../list.txt',__FILE__)) do |f|
+Service.primary.post(:message =>"#{"@" + m.user.idname} がんばって！\n" + f.readlines[rand(290)], :replyto => m)
+end     
+   end
+#ゆいおぐらガチャ(10連)
+if m.message.to_s =~ /ゆいおぐら10連ガチャ/
+10.times { 
+open(File.expand_path('../list.txt',__FILE__)) do |f|
+Service.primary.post(:message =>"#{"@" + m.user.idname} " + f.readlines[rand(290)], :replyto => m)
+end
+sleep 1
 }
-}
-putphoto = sol[num]
-Service.primary.post(:message =>"#{"@" + m.user.idname} " + putphoto.to_s, :replyto => m)
-        end
-    end
-  end
+	    
+   end
+
+end
+end
 end
 end
